ccd4e202966341c58ac2dcc289c8f320da318023
tjy511 <tjy22@cam.ac.uk>
2016-10-24 15:48:21 +0100

    conversion of layers to polar coords to identify slope

--- layerSlope.m
+++ layerSlope.m
@@ -13,31 +13,33 @@ zz = pp_slicey;
 
 % Plot unaltered profile
 plotimgprofile_gland(xx,yy,zz);
-title(['Vertical 2D profile (x-direction) at Date/Time: ', datestr(dateStamp)])
+title(['Vertical 2D profile (y-direction) at Date/Time: ', datestr(dateStamp)])
 
-%% Filter profile
+%% 1. Filter profile
 
 cfg.filter = 1; % Switch to filter/smooth profile
 cfg.ftype = 'gaussian'; % Rotationally symmetric Gaussian lowpass filter
 cfg.fsize = 7; % Filter size
 cfg.fsigma = 1; % Standard deviation threshold
+cfg.fwindow = 2; % Window size in convolution
 
 % Apply filters to remove noise
 if cfg.filter
     filt = (fspecial(cfg.ftype,cfg.fsize,cfg.fsigma)); % Guassian lowpass filter 
     thres = (max([min(max(zz,[],1))  min(max(zz,[],2))])) ;
-    zz = medfilt2(zz,[3,3]); % Median filtering in 2 directions
+    zz = medfilt2(zz,[cfg.fwindow,cfg.fwindow]); % Median filtering in 2 directions
     zz = conv2(zz,filt,'same'); % 2-D convolution with designed filter
 end
 
-%% Identify peaks in 1D --> 2D
+%% 2. Identify layers
 
 cfg.thresh = -50; % Power threshold for peaks
+cfg.vecLength = 3; % Minimum vector length of layers
 
 % Pre-allocate arrays
 int.pks = nan(size(xx)); int.locs = int.pks;
 
-% Find all peaks
+% Find all peaks (1D --> 2D)
 for ii = 1:size(xx,2)
     vec = zz(:,ii); % Depth vector of power
     [pks,locs] = findpeaks(vec,'minPeakHeight',db2mag(cfg.thresh)); % Identify peaks
@@ -59,21 +61,46 @@ for ii = 1:size(xx,2)
     end
 end
 
-% Plotting fancies
-plotimgprofile_gland(xx,yy,zz);
-plot3(xxPk,yyPk,zzPk,'k.')
+% % Plotting fancies
+% plotimgprofile_gland(xx,yy,zz);
+% plot3(xxPk,yyPk,zzPk,'k.')
 
-%% Identify connected components (layers)
+% Identify connected components (layers)
 BW = ~isnan(zzPk);
 CC = bwconncomp(BW); 
+layers.idx = CC.PixelIdxList;
 
-% Remove vectors shorter than n pixels
+% To do: Remove vectors shorter than n pixels
+for ii = 1:CC.NumObjects % Replaces short vectors with NaN
+    if size(layers.idx{ii},1) < cfg.vecLength
+        layers.idx{ii} = NaN; 
+    end
+end
+fh = @(x) all(isnan(x(:)));
+layers.idx(cellfun(fh,layers.idx)) = []; % Removes NaN values from cell array
+CC.NumObjects = length(layers.idx); % Re-calculate array length
 
 % Plotting fancies
 plotimgprofile_gland(xx,yy,zz);
 for ii = 1:CC.NumObjects
-    layer = CC.PixelIdxList{ii};
-    plot3(xxPk(layer),yyPk(layer),zzPk(layer),'k')
+    layer = layers.idx{ii};
+    x = xxPk(layer); y = yyPk(layer); z = zzPk(layer);
+    plot3(x,y,z,'k')
+end
+
+%% 3. Identify slope of layers
+
+for ii = 1:CC.NumObjects
+    layer = layers.idx{ii};
+    
+    % Fit regression
+    x = xxPk(layer); y = yyPk(layer); 
+    %breg = robustfit(x,y);
+    breg = regress(y,[ones(length(x),1) x]);
+    
+    % Re-assign output to polar coordinates
+    layers.r(ii) = sqrt(range(x)^2+range(y)^2); % Rho
+    layers.t(ii) = atand(range(y)/range(x)); % Theta
 end
 
 %% SCRATCH
